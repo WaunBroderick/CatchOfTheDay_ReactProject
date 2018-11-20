183c1853d988db456c3c9bd0dcc7e4807f36d23b
WaunBroderick <waunbroderick@gmail.com>
2018-11-20 13:00:53 -0500

    Updated the App component and created the Header class

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+
+class App extends React.Component {
+    render(){
+        return(
+            <div className="catch-of-the-day">
+            <div class="menu">
+            <Header/>
+            </div>
+            {/* <INVENTORY> */}
+            {/* <ORDER> */}
+            </div>
+        )
+    }
+}
+
+export default App;
